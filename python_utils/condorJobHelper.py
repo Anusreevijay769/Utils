b88aca5e5f881de664da485326967785fa93cf48
ram1123 <ramkrishna.sharma71@gmail.com>
2020-05-12 16:29:27 -0500

    updated jdl creater method

--- python_utils/condorJobHelper.py
+++ python_utils/condorJobHelper.py
@@ -3,12 +3,20 @@ import sys
 
 class condorJobHelper(object):
     """docstring for condorJobHelper"""
-    def __init__(self, fileName="test", listOfFilesToTransfer="", request_memory=0, request_cpus=0, Arguments="", Queue=1):
+    def __init__(self, fileName="test",
+                 listOfFilesToTransfer="",
+                 request_memory=0,
+                 request_cpus=0,
+                 logFilePath = 'test',
+                 logFileName = 'test',
+                 Arguments="",
+                 Queue=1):
         self.fileName = fileName
         self.listOfFilesToTransfer = listOfFilesToTransfer
         self.request_memory = request_memory
         self.request_cpus = request_cpus
         self.logFilePath = logFilePath
+        self.logFileName = logFileName
         self.Arguments = Arguments
         self.Queue = Queue
 
@@ -19,17 +27,26 @@ class condorJobHelper(object):
         outJdl.write('\n'+'Notification = ERROR')
         outJdl.write('\n'+'Should_Transfer_Files = YES')
         outJdl.write('\n'+'WhenToTransferOutput = ON_EXIT')
-        outJdl.write('\n'+'Transfer_Input_Files = '+condor_file_name+'.sh ' + self.listOfFilesToTransfer)
+        outJdl.write('\n'+'Transfer_Input_Files = '+self.fileName+'.sh, ' + self.listOfFilesToTransfer)
         outJdl.write('\n'+'x509userproxy = $ENV(X509_USER_PROXY)')
-        if (self.request_memory != 0) outJdl.write('\n'+'request_memory = '+self.request_memory)
-        if (self.request_cpus != 0) outJdl.write('\n'+'request_cpus = '+ self.request_cpus)
-        outJdl.write('\n'+'Output = '+self.logFilePath+'.stdout')
-        outJdl.write('\n'+'Error  = '+self.logFilePath+'.stdout')
-        outJdl.write('\n'+'Log  = '+self.logFilePath+'.log')
-        outJdl.write('\n'+'Arguments = $(Cluster) $(Process) '+self.Arguments)
-        outJdl.write('\n'+'Queue '+self.Queue)
-        outJdl.close()
+        if self.request_memory != 0: outJdl.write('\n'+'request_memory = '+str(self.request_memory))
+        if self.request_cpus != 0: outJdl.write('\n'+'request_cpus = '+ str(self.request_cpus))
+        # outJdl.write('\n'+'Output = '+self.logFilePath+'.stdout')
+        # outJdl.write('\n'+'Error  = '+self.logFilePath+'.stdout')
+        # outJdl.write('\n'+'Log  = '+self.logFilePath+'.log')
+        # outJdl.write('\n'+'Arguments = $(Cluster) $(Process) '+self.Arguments)
+        # outJdl.write('\n'+'Queue '+self.Queue)
+        # outJdl.close()
         return self.fileName+'.jdl'
+
+    def jdlFileAppendLogInfo(self):
+        outJdl = open(self.fileName+'.jdl','a')
+        outJdl.write('\n'+'Output = '+self.logFilePath+os.sep+self.logFileName+'.stdout')
+        outJdl.write('\n'+'Error  = '+self.logFilePath+os.sep+self.logFileName+'.stdout')
+        outJdl.write('\n'+'Log  = '+self.logFilePath+os.sep+self.logFileName+'.log')
+        outJdl.write('\n'+'Arguments = $(Cluster) $(Process) '+self.Arguments)
+        outJdl.write('\n'+'Queue '+str(self.Queue))
+        outJdl.close()
 
     def shFileCreater(self):
         outScript = open(self.fileName+".sh","w");
@@ -38,6 +55,14 @@ class condorJobHelper(object):
         outScript.write('\n'+'echo "Running on: `uname -a`"')
         outScript.write('\n'+'echo "System software: `cat /etc/redhat-release`"')
         outScript.write('\n'+'source /cvmfs/cms.cern.ch/cmsset_default.sh')
+        outScript.write('\n'+'echo "'+'#'*51+'"')
+        outScript.write('\n'+'echo "#    List of Input Arguments: "')
+        outScript.write('\n'+'echo "'+'#'*51+'"')
+        outScript.write('\n'+'echo "Input Arguments: $1" ')
+        outScript.write('\n'+'echo "Input Arguments: $2" ')
+        for x in xrange(3,len(self.Arguments)+3):
+            outScript.write('\n'+'echo "Input Arguments: $'+x+'" ')
+        outScript.write('\n'+'echo "'+'#'*51+'"')
         outScript.write('\n'+'')
         outScript.close()
         return self.fileName+'.sh'
